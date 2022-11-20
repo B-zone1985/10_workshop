de29c92bfe2614676a682b7b21a918d295210788
Олег Курков <Kurkov_o@infars.ru>
2022-11-20 20:07:47 +0300

    Добавьте файлы проекта.

---
+++ 10_workshop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10_workshop
+{
+    class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            var worker = new Worker(Logger);
+            worker.Work();
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface ILogger /// определяем наш интерфейс
+    {
+        void Summ(string message); /// добавляем метод
+    }
+
+    public class Logger : ILogger /// создаем класс, который будет реализовывать не явно ILogger
+    {
+
+        public void Summ(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IWorker /// определяем интерфейс . что будет делать наш класс Summ
+    {
+        void Work(); /// исполнение метода
+    }
+}
---
+++ 10_workshop/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _10_workshop
+{
+    public class Worker : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            try
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Logger.Summ("ВВедите число 1: ");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Logger.Summ("Введите число 2: ");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                int summ = numberOne + numberTwo;
+                Console.WriteLine("Сумма двух чисел равна: \n{0}", summ);
+            }
+            catch(FormatException)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введено не корректное значение. Введите число!");
+            }
+        }
+    }
+
+}
